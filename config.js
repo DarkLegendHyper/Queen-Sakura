655dfa8bd8b6d08eb9c68944b6c5a8ea74c11be8
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-07-22 18:22:24 +0530

    🧚‍♂️┃ＱＵＥＥＮ-ＳＡＫＵＲＡ┃🧚‍♂️

---
+++ config.js
@@ -0,0 +1,99 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+
+//
+global.available = true;
+global.autoReadGc = true;
+global.autoReadAll = false;
+global.antitags = true;
+
+
+//
+global.Owner = ["94770378874","94770378874"]; //If you want singal number so global.Owner = ['94770378874'] Change into your number
+global.OwnerNumber = ["94770378874","94770378874"];  //If you want singal number so global.Owner = ['94770378874'] Change into your number
+global.ownertag = ["94770378874"];
+global.BotName = "🧚‍♂️QUEEN-SAKURA-MD🧚‍♂️";
+global.packname = "🧚‍♂️QUEEN-SAKURA-MD🧚‍♂️";
+global.author = "©Created by:SUNETH";
+global.OwnerName = "Suneth";
+global.BotSourceCode = "https://github.com/blacksyberzizuko/Queen-Sakura";
+global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
+global.sessionName = "session";
+
+
+//  Prefix  //
+global.prefa = [".","-","/","#","_"];
+
+
+//
+global.location = "ANURADHAPURA";
+global.reactmoji = "👋";
+global.themeemoji = "🎭";
+global.vidmenu = { url: 'https://media.tenor.com/Jdu0Ov8X2sIAAAAC/A17-Bot.mp4' };
+global.websitex = "https://github.com/blacksyberzizuko";
+global.lolhuman = "KaysaS";
+
+//
+global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
+global.Thumb = fs.readFileSync("./Assets/pic9.jpg");
+global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
+global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
+
+
+//
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+//
+global.mess = {
+    jobdone: '🧚‍♂️┃work done...',
+    useradmin: '🧚‍♂️┃Sorry, only *Group Admins* can use this command !',
+    botadmin: '🧚‍♂️┃Sorry, i cant execute this command without being an *Admin* of this group.',
+    botowner: '🧚‍♂️┃Only my *Owner* can use this command, !',
+    grouponly: '🧚‍♂️┃This command is only made for *Groups*, ',
+    privateonly: '🧚‍♂️┃This command is only made for *Private Chat*, ',
+    botonly: '🧚‍♂️┃Only the *Bot itself* can use this command!',
+    waiting: '🧚‍♂️┃⏳ please wait ......',
+    nolink: '🧚‍♂️┃Please provide me *link*, ',
+    error: '🧚‍♂️┃An error occurd!',
+    banned: '🧚‍♂️┃You are *Banned* fron using commands!',
+    bangc: '🧚‍♂️┃This Group is *Banned* from using Commands!',
+    nonsfw: '🧚‍♂️┃Dont be a pervert Baka! This is not a NSFW enabled group!'
+    
+}
+
+global.limitawal = {
+  premium: "Infinity",
+  free: 2,
+  monayawal: 1000,
+};
+
+global.limitawal = {
+  rakyat: "Infinity",
+  free: 100,
+};
+
+global.APIs = {
+  zenz: "https://zenzapis.xyz",
+};
+global.APIKeys = {
+  "https://zenzapis.xyz": "5d1197db351b",
+};
