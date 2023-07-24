8237942b4993c9c51a795dd5b12c8e076c73b112
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-07-24 18:13:50 +0530

    🧚‍♂️┃ＱＵＥＥＮ-ＳＡＫＵＲＡ ┃🧚‍♂️

--- config.js
+++ config.js
@@ -35,10 +35,10 @@ global.websitex = "https://github.com/blacksyberzizuko";
 global.lolhuman = "KaysaS";
 
 //
-global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
-global.Thumb = fs.readFileSync("./Assets/pic9.jpg");
-global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
-global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
+global.BotLogo = fs.readFileSync("./Assets/Queen-Sakura_1.jpg");
+global.Thumb = fs.readFileSync("./Assets/Queen-Sakura_1.jpg");
+global.Thumb1 = fs.readFileSync("./Assets/Queen-Sakura_1.jpg");
+global.ErrorPic = fs.readFileSync("./Assets/Queen-Sakura_2.jpg");
 
 
 //
